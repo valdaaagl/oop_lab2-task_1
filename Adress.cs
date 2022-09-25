30de3443e6ec602de87246327bb27349bd8fbdd4
vladaaagl <vsh_o@ukr.net>
2022-09-25 15:57:45 +0300

    First task of the second lab

---
+++ Adress.cs
@@ -0,0 +1,70 @@
+class Adress
+{
+    public int index;
+    public int Index{
+        get
+        {
+            return this.index;
+        }
+        set
+        {
+            index = value;
+        }
+    }
+    public string country;
+    public string Country{
+        get
+        {
+            return this.country;
+        }
+        set
+        {
+            country = value;
+        }
+    }
+    public string city;
+    public string City{
+        get
+        {
+            return this.city;
+        }
+        set
+        {
+            city = value;
+        }
+    }
+    public string street;
+    public string Street{
+        get
+        {
+            return this.street;
+        }
+        set
+        {
+            street = value;
+        }
+    }
+    public int house;
+    public int House{
+        get
+        {
+            return this.house;
+        }
+        set
+        {
+            house = value;
+        }
+    }
+    public int apartment;
+    public int Apartment{
+        get
+        {
+            return this.apartment;
+        }
+        set
+        {
+            apartment = value;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Adress home = new Adress();
+home.Index = 08801;
+home.Country = "Україна";
+home.City = "Богуслав";
+home.Street = "Вишнева";
+home.House = 23;
+home.Apartment = 1;
+
+Console.WriteLine($"Моя адреса: вул. {home.Street}, буд. {home.House}, кв. {home.Apartment}, мiсто {home.City}, {home.Country}, {home.Index}");
